9992eb38e1f76929f8ee6cfde67b939bffd1baba
Your Name <xi03hy@gmail.com>
2022-07-08 06:38:05 +0900

    first commit

---
+++ src/Board.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import RatingForm from './RatingForm';
+
+const Board = () => {
+    return (
+        <div>
+            <RatingForm></RatingForm>
+        </div>
+    );
+};
+
+export default Board;
---
+++ src/RatingForm.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './RatingForm.css';
+
+const RatingForm = ({children}) => {
+    return (
+        <>
+        <div>TODOLIST</div>
+        <div>{children}</div>
+        </>
+    );
+};
+
+export default RatingForm;
